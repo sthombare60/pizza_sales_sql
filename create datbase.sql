b2f3af7f45539a37cf710dc9e0400ffcc2691520
sthombare60 <101254055+sthombare60@users.noreply.github.com>
2024-10-04 15:31:14 +0530

    Add files via upload

---
+++ create datbase.sql
@@ -0,0 +1,7 @@
+create database pizzahut;
+create table order_details (
+order_details_id int not null,
+order_id int not null,
+pizza_id text not null,
+quantity int not null,
+primary key(order_details_id) );
